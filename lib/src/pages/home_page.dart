2a6511715bf6047e773ef64ad181b14d6305ef86
Daniel Carmona <dcarmona808@gmail.com>
2020-11-08 19:39:54 -0400

    Stream y pushnamed arguments

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas/src/providers/peliculas_provider.dart';
 import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+import 'package:peliculas/src/widgets/movie_horizontal.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,6 +13,9 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+
+    peliculasProvider.getPopulares();
+
     return Scaffold(
         appBar: AppBar(
           title: Text('Peliculas en Cines'),
@@ -25,7 +29,11 @@ class _HomePageState extends State<HomePage> {
         ),
         body: Container(
           child: Column(
-            children: <Widget>[_swiperTarjeta()],
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              _swiperTarjeta(),
+              _footer(context)
+              ],
           ),
         ));
   }
@@ -46,6 +54,31 @@ class _HomePageState extends State<HomePage> {
           );
         }
       },
+    );
+  }
+
+  Widget _footer(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(left: 20.0),
+            child: Text('Populares', style: Theme.of(context).textTheme.subtitle1,)),
+          SizedBox(height: 5.0),
+          StreamBuilder(
+            stream: peliculasProvider.popularesStream,
+            builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+              if(snapshot.hasData){
+                return MovieHorizontal(peliculas: snapshot.data, siguientePagina: peliculasProvider.getPopulares,);
+              }else{
+                return Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
+        ],
+      )
     );
   }
 }
